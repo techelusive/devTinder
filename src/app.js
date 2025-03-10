31b4cb88fe56cbe1771b3bf118b3134f82add2d0
singhrohit-coder <iknowrohit19@gmail.com>
2025-03-10 10:24:55 +0530

    exploring get, post, patch  and delete HTTP methods

--- src/app.js
+++ src/app.js
@@ -5,9 +5,32 @@ const express = require("express");
 // call this express function
 const app = express();
 
+// this will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({
+        firstName: "Rohit",
+        lastName: "Singh"
+    });
+});
+
+app.post("/user", (req, res) => {
+    // save data to the database
+    res.send("Data successfully saved to the database");
+});
+
+app.patch("/user", (req, res) => {
+    res.send("Chnages updated");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data dalete successfully");
+});
+
+// this will match all the HTTP method API calls to /run
 app.use("/run", (req, res) => {
     res.send("Hello form server");
 });
+
 
 // listen incoming requests on port
 app.listen(7770, () => {
