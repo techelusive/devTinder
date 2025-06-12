26f3cf7733ee6922bb41ad4308bcdff50f2d6103
singhrohit-coder <iknowrohit19@gmail.com>
2025-06-13 01:58:55 +0530

    validation logic

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,17 +1,18 @@
 const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const {firstName, lastName, emailId, password} = req.body;
+  // extract the data want to validate from the body.
+  const { firstName, lastName, emailId, password } = req.body;
 
-    if (!firstName || !lastName) {
-        throw new Error("Names is not valid");
-    } else if (!validator.isEmail(emailId)) {
-        throw new Error("Email is not valid");
-    } else if (!validator.isStrongPassword(password)) {
-        throw new Error("Password is not strong");
-    }
+  if (!firstName || !lastName) {
+    throw new Error("Names is not valid");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email: thoda mehnet karle");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password: isme bhi mehnet nahi hui");
+  }
 };
 
 module.exports = {
-    validateSignUpData,
+  validateSignUpData,
 };
