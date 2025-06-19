04d4bbc55b5889afdaebb72ffc78f99d548d4060
singhrohit-coder <iknowrohit19@gmail.com>
2025-06-20 03:13:53 +0530

    chapter 10 complete

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,15 +1,31 @@
-const adminAuth = (req, res, next) => {
-  console.log("user auth is getting checked");
-  const token = "xyz";
-  const isAdminAuthorised = token === "xyz";
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-  if (!isAdminAuthorised) {
-    res.status(401).send("unauthorised user");
-  } else {
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from the cookie
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    // Validate the token
+    const decodeObj = await jwt.verify(token, "GURUJIKIJAY@19");
+    // get the user id from the token
+    const { _id } = decodeObj;
+    // find the user using id.
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("user not found");
+    }
+
+    // attach user with the req.user
+    req.user = user;
     next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
 
 module.exports = {
-  adminAuth,
+  userAuth,
 };
